56375d95f674b43c5ae27c9e551cbac161b97503
sivach <sivagopal.chavali@appliedis.com>
2016-05-28 22:46:53 +0530

    One Type class name change and online code commit.

--- GitDemoConsoleApp/GitDemoConsoleApp/OneClass.cs
+++ GitDemoConsoleApp/GitDemoConsoleApp/OneClass.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace GitDemoConsoleApp
 {
-    class OneClass
+    class OneTypeClass
     {
         //Added new class from branch1.
         //Line to test multiple file conflicts.
         //Edited in online Git master branch.
         //One more online commit and class name change.
+        //One Type class name change and online code commit.
     }
 }
